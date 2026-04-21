6676c9909378ab26cb390c51e2d201dd09c3ee7d
JossMikee <josemiguelvazquezsanchez682@gmail.com>
2026-04-21 15:36:19 -0600

    Actualizacion

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request
 import os
-from processing.utils import save_wav, read_wav
+from processing.utils import save_wav, read_audio, generar_grafica
 from processing.laplace import aplicar_laplace
 from processing.edo import filtro_edo
 
 app = Flask(__name__)
 UPLOAD_FOLDER = 'audio'
+GRAPH_FOLDER = 'static/graphs'
+
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+os.makedirs(GRAPH_FOLDER, exist_ok=True)
 
 @app.route('/')
 def index():
@@ -22,7 +25,7 @@ def upload():
 
     file.save(input_path)
 
-    rate, data = read_wav(input_path)
+    rate, data = read_audio(input_path)
 
     if metodo == 'laplace':
         filtered = aplicar_laplace(rate, data)
@@ -31,7 +34,18 @@ def upload():
 
     save_wav(output_path, rate, filtered)
 
-    return send_file(output_path, as_attachment=True)
+    # Generar gráficas
+    grafica_input = os.path.join(GRAPH_FOLDER, 'input.png')
+    grafica_output = os.path.join(GRAPH_FOLDER, 'output.png')
+
+    generar_grafica(data, grafica_input, 'Señal Original')
+    generar_grafica(filtered, grafica_output, 'Señal Filtrada')
+
+    return render_template('index.html',
+                           input_audio=input_path,
+                           output_audio=output_path,
+                           grafica_input=grafica_input,
+                           grafica_output=grafica_output)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- processing/utils.py
+++ processing/utils.py
@@ -1,11 +1,39 @@
+import os
 import numpy as np
 from scipy.io import wavfile
+import matplotlib.pyplot as plt
 
 
-def read_wav(path):
-    rate, data = wavfile.read(path)
-    return rate, data.astype(np.float32)
+def read_audio(path):
+    ext = os.path.splitext(path)[1].lower()
+
+    if ext == ".wav":
+        rate, data = wavfile.read(path)
+    else:
+        audio = AudioSegment.from_file(path)
+        temp_path = "audio/temp.wav"
+        audio.export(temp_path, format="wav")
+        rate, data = wavfile.read(temp_path)
+
+    data = data.astype(np.float32)
+
+    # 🔥 CONVERTIR A MONO SI ES ESTÉREO
+    if len(data.shape) == 2:
+        data = np.mean(data, axis=1)
+
+    return rate, data
 
 
 def save_wav(path, rate, data):
     wavfile.write(path, rate, data.astype(np.int16))
+
+
+def generar_grafica(data, path, titulo):
+    plt.figure()
+    plt.plot(data)
+    plt.title(titulo)
+    plt.xlabel('Tiempo')
+    plt.ylabel('Amplitud')
+    plt.savefig(path)
+    plt.close()
+    
